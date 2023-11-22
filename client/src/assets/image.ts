5882cbda19dffaacd3b0c1920917e296a0aae1bf
ripeplantain <irvingmanny@gmail.com>
2023-11-22 08:34:08 +0000

    done with login page

---
+++ client/src/assets/image.ts
@@ -0,0 +1,8 @@
+import Login from './image/calin-stan-7a_PHX91su8-unsplash.jpg'
+import Logo from './image/logo.png'
+
+
+
+export {
+    Login, Logo
+}
---
+++ client/src/helpers/validation.ts
@@ -0,0 +1,25 @@
+import * as yup from "yup";
+
+
+
+const loginSchema = yup
+    .object({
+        email: yup.string().required("Email is required").email("Email is invalid"),
+        password: yup.string().required("Password is required").min(10, "Password must be at least 6 characters")
+    })
+
+
+const registerSchema = yup
+    .object({
+        firstName: yup.string().required("First Name is required").min(2, "First Name must be at least 2 characters"),
+        lastName: yup.string().required("Last Name is required").min(2, "Last Name must be at least 2 characters"),
+        address: yup.string().required("Address is required").min(2, "Address must be at least 2 characters"),
+        phoneNumber: yup.string().required("Phone Number is required").min(10, "Phone Number must be at least 10 characters"),
+        email: yup.string().required("Email is required").email("Email is invalid"),
+        password: yup.string().required("Password is required").min(10, "Password must be at least 6 characters"),
+        confirmPassword: yup.string().required("Confirm Password is required").oneOf([yup.ref("password")], "Passwords must match")
+    })
+
+
+
+export { loginSchema, registerSchema }
